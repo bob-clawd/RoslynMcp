7f7295fc6f7c17590cd5628ac431efade2c97641
chrismo80 <krizm0@freenet.de>
2026-03-12 11:12:42 +0100

    remove dummy logger

--- src/RoslynMcp.Host/McpServerHost.cs
+++ src/RoslynMcp.Host/McpServerHost.cs
@@ -8,14 +8,8 @@ public static class McpServerHost
     public static async Task RunAsync(string[] args, CancellationToken cancellationToken = default)
     {
         var builder = Microsoft.Extensions.Hosting.Host.CreateApplicationBuilder(args);
-        builder.Logging.ClearProviders();
-        //TODO: Replace the default logger with a robust logging provider (Serilog or NLog) and avoid using stdio, as it interferes with the MCP protocol.
-        //builder.Logging.AddSimpleConsole(consoleOptions =>
-        //{
-        //    consoleOptions.SingleLine = true;
-        //    consoleOptions.TimestampFormat = "[yyyy-MM-dd HH:mm:ss] ";
-        //});
 
+        builder.Logging.ClearProviders();
         builder.Services.Compose();
 
         var host = builder.Build();
